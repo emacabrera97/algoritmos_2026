9a415c176030a65825f4c579bff650a38e2109cc
Emanuel <emanuelncabrera97@gmail.com>
2026-03-17 17:50:28 -0300

    primer commit

---
+++ recursividad.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+def fibonacci(n):
+    ...
+
+print(factorial(num))
+print(fibonacci(num))
+
