8e24d8d6bde79cf8234e19cf7d2be9ba0d814fa0
Emanuel <emanuelncabrera97@gmail.com>
2026-03-22 16:47:34 -0300

    estructura para ejecucion armada

--- recursividad.py
+++ recursividad.py
@@ -1,13 +1,32 @@
-num = int(input())
+import sys
+
 
 def factorial(n):
     if n == 0:
         return 1
     return n * factorial(n-1)
 
+
 def fibonacci(n):
-    ...
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return(fibonacci(n-1) + fibonacci(n-2))
 
-print(factorial(num))
-print(fibonacci(num))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        func = input("Funcion a evaluar: ")
+        n = input("Valor a evaluar: ")
+    if len(sys.argv) == 3:
+        func = sys.argv[1]
+        n = sys.argv[2]
+    else:
+        sys.exit("Error: Cantidad de parametros incorrecta")
 
+    match func:
+        case "factorial":
+            print(factorial(int(n)))
+        case "fibonacci":
+            print(fibonacci(int(n)))
